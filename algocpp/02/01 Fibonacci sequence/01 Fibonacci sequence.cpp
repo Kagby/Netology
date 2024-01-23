cc4d6dc7bff67501ad36bd333ad9e564f7c48658
Kagby <kagby@yandex.ru>
2024-01-23 22:12:46 +0500

    Add 02/01 Fibonacci sequence

---
+++ algocpp/02/01 Fibonacci sequence/01 Fibonacci sequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+
+    std::cout << "Введите число: ";
+    std::cin >> n;
+
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < n; ++i)
+        std::cout << fib(i) << " ";
+
+    std::cout << std::endl;
+
+    return 0;
+}
